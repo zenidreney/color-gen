ea3dcbde56b3b90caa3586202b2a8b69cfbea34c
Deniz <auo@tuta.io>
2025-07-05 20:03:13 +0200

    added copy color functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputColor = document.getElementById("input-color");
 const colorMode = document.getElementById("color-mode");
 const getColorBtn = document.getElementById("get-color");
+const outputContainer = document.getElementById("output-container");
 
 let userMode = "monochrome";
 let userColor = "#F55A5A";
@@ -13,19 +14,37 @@ document.addEventListener("input", (e) => {
     }
 });
 
+/*Copy to Clipboard*/
+
+outputContainer.addEventListener("click", (e) => {
+    console.log("Clicked");
+
+    if (e.target.id.startsWith("color-")) {
+        const index = e.target.id.split("-")[1];
+
+        //console.log(document.getElementById(`color-${index}`).nextElementSibling.textContent);
+
+        const copyHex = document.getElementById(`color-${index}`).nextElementSibling.textContent;
+
+        navigator.clipboard.writeText(copyHex);
+        alert("Copied");
+    }
+});
+
+/*Fetch Data from Color API*/
+
 getColorBtn.addEventListener("click", () => {
     fetch(`https://www.thecolorapi.com/scheme?hex=${userColor.slice(1)}&mode=${userMode}&count=4`)
         .then((res) => res.json())
         .then((data) => {
-        
-        /*Collect the colors*/
+            /*Collect the colors*/
             const userScheme = [userColor];
-        
-                for (let i = 0; i < data.colors.length; i++) {
-                    userScheme.push(data.colors[i].hex.value);
-                }
-        
-        /*Update the DOM with DOM API*/
+
+            for (let i = 0; i < data.colors.length; i++) {
+                userScheme.push(data.colors[i].hex.value);
+            }
+
+            /*Update the DOM with DOM API*/
             const outputContainer = document.getElementById("output-container");
 
             outputContainer.innerHTML = "";
