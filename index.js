0bf29b337f7d1a62ffd8cf71999fb3fb448757a9
Deniz <auo@tuta.io>
2025-07-06 19:31:50 +0200

    Corrected logic for confirmation box

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ document.addEventListener("input", (e) => {
 outputContainer.addEventListener("click", (e) => {
     console.log("Clicked");
 
-    if (e.target.id.startsWith("color-") || (e.target.id.startsWith("copy-"))) {
+    if (e.target.id.startsWith("color-") || e.target.id.startsWith("copy-")) {
         const index = e.target.id.split("-")[1];
 
         //console.log(document.getElementById(`color-${index}`).nextElementSibling.textContent);
@@ -31,17 +31,14 @@ outputContainer.addEventListener("click", (e) => {
         const copyHex = document.getElementById(`color-${index}`).nextElementSibling.textContent;
 
         navigator.clipboard.writeText(copyHex);
-        //alert("Copied");
-        
-        
+
+        //Confirm UI
+
         document.getElementById("copy-confirm").classList.add("visible");
-        
-        setTimeout(function(){
-            
-           document.getElementById("copy-confirm").classList.remove("visible"); 
-            
+
+        setTimeout(function () {
+            document.getElementById("copy-confirm").classList.remove("visible");
         }, 2000);
-        
     }
 });
 
@@ -51,7 +48,6 @@ getColorBtn.addEventListener("click", () => {
     fetch(`https://www.thecolorapi.com/scheme?hex=${userColor.slice(1)}&mode=${userMode}&count=4`)
         .then((res) => res.json())
         .then((data) => {
-        
             /*Collect the colors*/
             const userScheme = [userColor];
 
@@ -64,28 +60,33 @@ getColorBtn.addEventListener("click", () => {
 
             outputContainer.innerHTML = "";
 
+            const copyConfirmText = document.createElement("p");
+            copyConfirmText.textContent = "Copied to Clipboard";
+            copyConfirmText.className = "copy-confirm";
+            copyConfirmText.id = "copy-confirm";
+
             userScheme.forEach((hex, i) => {
                 const innerDiv = document.createElement("div");
 
-                    const outputColorDiv = document.createElement("div");
-                    outputColorDiv.id = `color-${i}`;
-                    outputColorDiv.className = "output-color";
-                    outputColorDiv.style.backgroundColor = hex;
-                
-                        const copySpan = document.createElement("span");
-                        copySpan.id = `copy-${i}`;
-                        copySpan.textContent = "COPY";
-                        copySpan.className = "copy";
-                    
-                    outputColorDiv.append(copySpan);
+                const outputColorDiv = document.createElement("div");
+                outputColorDiv.id = `color-${i}`;
+                outputColorDiv.className = "output-color";
+                outputColorDiv.style.backgroundColor = hex;
 
-                    const hexNumber = document.createElement("p");
+                const copySpan = document.createElement("span");
+                copySpan.id = `copy-${i}`;
+                copySpan.textContent = "COPY";
+                copySpan.className = "copy";
 
-                    hexNumber.textContent = hex;
+                outputColorDiv.append(copySpan);
+
+                const hexNumber = document.createElement("p");
+
+                hexNumber.textContent = hex;
 
                 innerDiv.append(outputColorDiv, hexNumber);
 
-                outputContainer.append(innerDiv);
+                outputContainer.append(copyConfirmText, innerDiv);
             });
         });
 });
